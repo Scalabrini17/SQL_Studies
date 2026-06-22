4277b7defe38a49003cc909f882cffa80c7568ae
Scalabrini17 <itallomartins2007@gmail.com>
2026-06-22 16:13:23 -0300

    Update: TechHub Consultas

--- TechHub/Consultas.sql
+++ TechHub/Consultas.sql
@@ -36,4 +36,34 @@ SELECT * FROM categorias;
 -------------------------------------------------------------------------------------------
 /*Consultas Avançadas*/
 --1
-SELECT * FROM pedidos INNER JOIN clientes ON pedidos.id_cliente = clientes.id_cliente;
+SELECT clientes.nome_cliente, pedidos.data_pedido, pedidos.status, pedidos.valor_total 
+FROM pedidos 
+INNER JOIN clientes ON pedidos.id_cliente = clientes.id_cliente;
+
+--2
+SELECT pedidos.id_pedido, produtos.nome_produto, itens_pedido.quantidade, itens_pedido.preco_unitario	
+FROM itens_pedido
+INNER JOIN pedidos ON itens_pedido.id_pedido = pedidos.id_pedido
+INNER JOIN produtos ON itens_pedido.id_produto = produtos.id_produto;
+
+--3 
+SELECT clientes.nome_cliente, SUM(Pedidos.valor_total) AS total_gastos 
+FROM pedidos
+INNER JOIN clientes ON pedidos.id_cliente = clientes.id_cliente
+GROUP BY clientes.nome_cliente
+ORDER BY clientes.nome_cliente ASC;
+
+--4
+SELECT clientes.nome_cliente
+FROM pedidos
+RIGHT JOIN clientes ON pedidos.id_cliente = clientes.id_cliente
+WHERE pedidos.id_pedido IS NULL ;
+
+--5
+SELECT produtos.nome_produto
+FROM itens_pedido
+RIGHT JOIN produtos ON itens_pedido.id_produto = produtos.id_produto
+WHERE itens_pedido.id_produto IS NULL;
+
+--6
+
